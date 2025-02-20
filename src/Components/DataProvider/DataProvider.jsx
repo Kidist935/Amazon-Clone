70a3ed7c3352b6b399ad8d19fa8d224b123ff915
Kidist935 <abate.kidist@gmail.com>
2025-02-19 22:53:27 -0600

    Add to cart functionality

---
+++ src/Components/DataProvider/DataProvider.jsx
@@ -0,0 +1,17 @@
+import React, {createContext, useReducer} from 'react'
+import { reducer } from '../../Utility/reduser'
+
+
+export const DataContext = createContext()
+
+export const DataProvider =({children,reducer, initialState}) =>{
+    return(
+
+    <DataContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+
+    </DataContext.Provider>
+
+    )
+}
+
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./header.module.css"
 import { Link } from "react-router-dom";
 import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
 import LowerHeader from "./LowerHeader";
+import { DataContext } from "../DataProvider/DataProvider";
 
 function Header() {
+
+ const [{basket}, dispatch]=useContext(DataContext)
+
   return (
     <>
       <section>
@@ -73,7 +77,7 @@ function Header() {
 
           <Link to="/cart" className={classes.cart}>
           <BiCart size={35}/>
-            <span>0</span>
+            <span>{basket.length}</span>
           </Link>
         </div>
         </div>
