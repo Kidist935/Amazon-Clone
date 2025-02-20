767c37bded7eafd35d279493c5fa7221bae4c6b1
Kidist935 <abate.kidist@gmail.com>
2025-02-20 00:37:39 -0600

    Cart part two

--- src/Utility/reduser.js
+++ src/Utility/reduser.js
@@ -9,10 +9,31 @@ export const initialState = {
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
-      return { 
-        ...state, basket: [...state.basket, action.item] 
-    };
+    // Check if the item already exists in the cart
+    const existingItem = state.basket.find((item) => item.id === action.item.id);
+    if (!existingItem) { return {
+      ...state, basket: [...state.basket, { ...action.item, amount: 1 }], 
+    
+    }; 
+  }else {
+    // If item exists, update its amount
+    const updatedBasket = state.basket.map((item) =>{
+     return item.id === action.item.id ? { ...item, amount: item.amount + 1 } : item
+
+    })
+    return { 
+      ...state, 
+      basket: updatedBasket
+     }; 
+    }
+
+
     default:
       return state;
   }
 };
+
+
+
+
+        
