fac6c3c21e82f629369b2860402b239ef71906fa
Kidist935 <abate.kidist@gmail.com>
2025-03-05 18:26:32 -0600

    Building payment page UI

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Landing from "./Pages/Landing/Landing"
-import Auth from "./Pages/Auth/Auth"
-import Payment from "./Pages/Payment/Payment"
-import Orders from "./Pages/Orders/Orders"
-import Cart from "./Pages/Cart/Cart"
-import ProductDetail from './Pages/ProductDetail/ProductDetail'
-import Result from "./Pages/Result/Result"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./Pages/Landing/Landing";
+import Auth from "./Pages/Auth/Auth";
+import Payment from "./Pages/Payment/Payment";
+import Orders from "./Pages/Orders/Orders";
+import Cart from "./Pages/Cart/Cart";
+import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import Result from "./Pages/Result/Result";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51Qxhzc06V7sjtmR1D5EEaSOAZtbuhloduBiYlrkxoSKfelffNZTmrB9HPNywLUrxa3VYchrdWWaqARXKdsBZuy2e00WmtHHLi7"
+);
 
 function Routeing() {
   return (
-   <Router>
-
+    <Router>
       <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/auth" element={<Auth/>}/>
-        <Route path="/payments" element={<Payment/>}/>
-        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
+        <Route path="/orders" element={<Orders />} />
         {/* categoryName is a dynamic parameter extracted using useParams */}
-        <Route path="/category/:categoryName" element={<Result/>}/>
-        <Route path="/products/:productId" element={<ProductDetail/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        
-        
-
-
+        <Route path="/category/:categoryName" element={<Result />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
-
-   </Router>
-
-
-  )
+    </Router>
+  );
 }
 
-export default Routeing
+export default Routeing;
